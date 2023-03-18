2e8abe2db8bb1f1ce4eb554162dd812d90499579
kqns91 <kansai.119917@gmail.com>
2023-03-18 22:01:17 +0900

    [Add] #4 メンバー情報を取得する処理を実装

--- nogi/member.go
+++ nogi/member.go
@@ -2,10 +2,26 @@ package nogi
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
 	"github.com/kqns91/nogi-sdk-go/nogi/model"
 )
 
 func (c *client) GetAllMembers(ctx context.Context) ([]*model.Member, error) {
-	panic("not implemented") // TODO: Implement
+	urlStr := fmt.Sprintf("%s://%s/%s/member", c.httpProtocol, baseURL, apiBasePath)
+
+	data, err := c.httpClient.send(ctx, http.MethodGet, urlStr, nil, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send: %w", err)
+	}
+
+	res := &model.Members{}
+	err = json.Unmarshal(data, &res)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal: %w", err)
+	}
+
+	return res.Data, nil
 }
